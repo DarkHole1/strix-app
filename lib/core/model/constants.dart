6f2c7a9c983dfd6b41edd4e87638852e9553fc7f
Dark Hole <dark.hole1@yandex.ru>
2025-02-05 21:08:46 +0300

    Removed constants

--- lib/core/model/constants.dart
+++ lib/core/model/constants.dart
@@ -1,15 +1,15 @@
 abstract class Constants {
   static const appName = "Strix";
-  static const githubUrl = "https://github.com/DarkHole1/strix-app";
+  static const githubUrl = " ";
   static const githubReleasesApiUrl =
-      "https://github.com/DarkHole1/strix-app/releases";
+      " ";
   static const githubLatestReleaseUrl =
-      "https://github.com/DarkHole1/strix-app/releases/latest";
+      "";
   static const appCastUrl =
-      "https://raw.githubusercontent.com/DarkHole1/strix-app/main/appcast.xml";
-  static const telegramChannelUrl = "https://t.me/strix";
-  static const privacyPolicyUrl = "https://strix.com/privacy-policy/";
-  static const termsAndConditionsUrl = "https://strix.com/terms/";
+      "";
+  static const telegramChannelUrl = "";
+  static const privacyPolicyUrl = "";
+  static const termsAndConditionsUrl = "";
   static const cfWarpPrivacyPolicy =
       "https://www.cloudflare.com/application/privacypolicy/";
   static const cfWarpTermsOfService =
